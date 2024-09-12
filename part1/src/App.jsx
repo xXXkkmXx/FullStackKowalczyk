8cc19df76d7ac51c779283a459f6b7662e9711a5
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2024-09-12 09:05:03 +0200

    done a part 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,28 @@
-const Header = (course,source2) => {
+const Header = (props) => {
   return(
     <div>
-        {course}{source2}
+        <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Upper = (props) => {
+  return(
+    <div>
+      {props.part1}:{props.exercises1}
+    </div>
+  )
+} 
+const Middle = (props) =>{
+  return(
+    <div>
+      {props.part2}:{props.exercises2}
+    </div>
+  )
+}
+const Bottom = (props) =>{
+  return(
+    <div>
+      {props.part3}:{props.exercises3}
     </div>
   )
 }
@@ -17,7 +38,10 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} source2={part1}/>
+      <Header course={course} />
+      <Upper part1={part1} exercises1={exercises1}/>
+      <Middle part2={part2} exercises2={exercises2}/>
+      <Bottom part3={part3} exercises3={exercises3}/>
     </div>
   )
 }

7f55d9e277247ed7105dbc3dcb2e5218c2b846d9
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2024-11-14 08:05:13 +0100

    not done yet

--- part1.14/src/App.jsx
+++ part1.14/src/App.jsx
@@ -1,42 +1,27 @@
 import { useState } from 'react'
 
-const Statistic = ({list}) =>{
-  let hlp = list[0] + list[1] + list[2];
-  let avrg;
-  let positive = list[0]/hlp*100;
-
-  avrg = hlp / list.length;
-  if(isNaN(positive)){
-    return (
-      <p>No feedback given</p>
-    )
-  }else{
-    return(
-      <div>
-        <p>good {list[0]}</p>
-        <p>neutral {list[1]}</p>
-        <p>bad {list[2]}</p>
-        <p>all {hlp}</p>
-        <p>average {avrg}</p>
-        <p>positive {positive}%</p>
-      </div>
-    )
-  }
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  let list = [good,neutral,bad];
-
+  let points = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0};
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const poinstCp = {...points};
   return (
     <div>
-      <h2>give feedback</h2>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
-      <Statistic list={list}/>
+      {anecdotes[selected]}
+      {points[selected]}
+      <p>
+        <button onClick={()=>setSelected(Math.round(Math.random()*8))}>next anecdotes</button>
+        <button onClick={()=>{poinstCp[selected] += 1;console.log(`points: ${points[selected]} pointsCp:${poinstCp[selected]}`)}}>vote</button>
+      </p>
     </div>
   )
 }

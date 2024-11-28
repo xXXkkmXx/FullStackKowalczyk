09083641d3ea9621872b8b944f847694328a076d
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2024-11-28 08:04:44 +0100

    added function

--- part2.10/src/App.jsx
+++ part2.10/src/App.jsx
@@ -6,27 +6,35 @@ const ShowPersons = ({props}) =>{
     names.push(props[i].name);
   }
   return (
-    names.map((values) => <h3>{values}</h3>)
+    names.map((values) => <h3 key={values}>{values}</h3>)
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas' } 
+  ]);
+  const [newName, setNewName] = useState('');
   
   const AddName = (event) =>{
-    event.preventDefaults();
-    console.log("button clicked",event.target);
+    event.preventDefault();
+    persons.push(newName);
+    setPersons(person => {name:{newName}});
   }   
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={AddName}>
         <div>
-          name: <input/>
+          name: 
+          <input
+            name="name"
+            type="text"
+            onChange={(e)=>{
+              setNewName(e.target.value);
+            }}
+          />
         </div>
         <div>
           <button type="submit" >add</button>

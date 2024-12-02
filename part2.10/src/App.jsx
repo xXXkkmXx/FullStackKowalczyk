a08aa5e419c4680215773ef7f84b6a4e98add1fe
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2024-12-02 09:54:10 +0100

    added a json server

--- part2.10/src/App.jsx
+++ part2.10/src/App.jsx
@@ -1,30 +1,63 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import  axios  from 'axios';
 
-const ShowPersons = ({props}) =>{
-  let names = [];
-  for(let i = 0;i < props.length;i++){
-    names.push(props[i].name);
+const ShowPersons = ({props,filter}) =>{
+  let names;
+  if(filter != ''){
+     names = [...props.filter(person => person.name.includes(filter))];
+  }else{
+     names = [...props];
   }
   return (
-    names.map((values) => <h3 key={values}>{values}</h3>)
+    names.map((values) => <h3 key={values.name}>{values.name} {values.number} <button onClick={Delete(values.id)}>delete</button></h3>)
   )
+} 
+
+const Delete = (id) =>{
+
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' } 
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
-  
+  const [newNumber, setNewNumber] = useState('');
+  const [newFilter,setNewFilter] = useState('');
+
   const AddName = (event) =>{
     event.preventDefault();
-    persons.push(newName);
-    setPersons(person => {name:{newName}});
+    persons.map(person=>{
+      if(person.name == newName){
+        alert(`${newName} is already added`);
+      }else{
+        // setPersons(persons => ([...persons,{name:newName,number:newNumber}]));  
+        const Data = {name:newName,number:newNumber,id:persons.lastIndexOf.id + 1};
+        axios.post('htpp://localhost:3001/persons')  
+      }
+    });
   }   
   
+  useEffect(()=>{
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response =>{
+      setPersons(response.data);
+    })
+  },[])
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with
+        <input
+          name="filter"
+          type="text"
+          onChange={(e)=>{
+            setNewFilter(e.target.value);
+          }} 
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={AddName}>
         <div>
           name: 
@@ -37,11 +70,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+          name="number"
+          type="text"
+          onChange={(e)=>{
+            setNewNumber(e.target.value);
+          }}
+          />
+        </div>
+        <div>
           <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ShowPersons props={persons}/>
+      <ShowPersons props={persons} filter={newFilter} />
     </div>
   )
 }

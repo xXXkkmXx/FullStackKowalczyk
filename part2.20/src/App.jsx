52aa1d1423abab59f80b6e533eced677e5ce642b
Maxymilian Kowalczyk <kowalczykm@tlimc.local>
2025-01-30 08:59:13 +0100

    code working but not working

--- part2.20/src/App.jsx
+++ part2.20/src/App.jsx
@@ -2,27 +2,56 @@ import axios from 'axios'
 import { useEffect } from 'react';
 import { useState } from 'react';
 
+const URL = `https://studies.cs.helsinki.fi/restcountries/api/all`;
 
+const OnSearch = ({countries,name}) => {
+  let specificCoun = [];
+  countries.map((item)=>{
+      if(item.name.common.includes(name) && name != '' && name.length > 1){
+        specificCoun.push(item);
+      }
+  });
+  return(
+    specificCoun.map((item)=>
+      <p key={item.name.common}>{item.name.common} <button onClick={(e)=>{Details(item)}}>show</button></p>
+    )
+  )
+}
+
+const Details = ({detailCountry}) =>{
+  return(
+    <div>
+      <h2>{detailCountry.name.common}</h2>
+      <p>capital:{detailCountry.capital}</p>
+      <p>area:{detailCountry.area}</p>
+      <h3>languages:</h3>
+      {
+        detailCountry.languages.map(item=><li>{item}</li>)
+      }
+    </div>
+  )
+}
 
 function App() {
   const [countries,setCountries] = useState([]);
+  const [nameCountry,setNameCountry] = useState('');
 
-  const OnSearch = () =>{
-      countries.map((datas) =>{
-        return <h3 key={datas.name.common}>{datas.name.official}</h3>
-      })
-  }
   useEffect(()=>{
-    axios.
-    get("https://studies.cs.helsinki.fi/restcountries/api/name/switzerland").
-    then(response => {
-      setCountries(countries.concat(response.data))
+    axios
+    .get(URL)
+    .then(response => {
+      setCountries(response.data);
     });
   },[]);
   return (
     <>
-     find countries: <input type='text'/>
-     <OnSearch/>
+     find countries: <input type='text'
+     onChange={(e)=>{
+      setNameCountry(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1));
+     }}
+     />
+     <OnSearch countries={countries} name={nameCountry}/>
+     
     </>
   )
 }
